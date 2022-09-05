dac310ce9ea9c48b16271f2b35d3e442fc3293f2
Mihail Tal <mihail.tal85@gmail.com>
2022-09-05 18:50:02 +0300

    HTML updated

--- Frequently_Asked_Questions/apps.js
+++ Frequently_Asked_Questions/apps.js
@@ -1,6 +1,6 @@
 const accordion =document.getElementsByClassName("content-container");
 
-for (let i=0; i< accordion.length; i++){
+for (i=0; i< accordion.length; i++){
     accordion[i].addEventListener("click",function(){
         this.classList.toggle("active");
     })
