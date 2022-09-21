9411ba3a39d666d0bffdee2ba7b1cee6ddc9ab87
Mihail Tal <mihail.tal85@gmail.com>
2022-09-21 09:51:05 +0300

    js and style css updated

--- Frequently_Asked_Questions/apps.js
+++ Frequently_Asked_Questions/apps.js
@@ -1,5 +1,6 @@
 const accordion =document.getElementsByClassName("content-container");
 
+//to change the classList to active when user click one part of the accordion
 for (i=0; i< accordion.length; i++){
     accordion[i].addEventListener("click",function(){
         this.classList.toggle("active");
