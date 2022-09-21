cf451a5fefc67bc2122de34f591e2095252bede4
Mihail Tal <mihail.tal85@gmail.com>
2022-09-21 09:23:57 +0300

    js updated

--- Checkout__Form_(HC-05)/checkout.js
+++ Checkout__Form_(HC-05)/checkout.js
@@ -3,12 +3,14 @@ let shoeMinus = document.querySelector("#shoeminus")
 let backbagPlus = document.querySelector("#bagplus")
 let backbagMinus = document.querySelector("#bagminus")
 
+
+//to add shoe price
 shoePlus.addEventListener("click", function(){
     document.getElementById("shoe").innerText = parseInt(document.getElementById("shoe").innerText)+1;
     document.getElementById("totalprice").innerText = `$${(parseInt(document.getElementById("shoe").innerText)*(74.99)+ parseInt(document.getElementById("bag").innerText)*(54.99)).toFixed(2) }`;
     price()
 })
-
+// to deduct shoe price
 shoeMinus.addEventListener("click", function(){
     if (parseInt(document.getElementById("shoe").innerText)>0) {
         document.getElementById("shoe").innerText = parseInt(document.getElementById("shoe").innerText)-1  ;
@@ -16,13 +18,13 @@ shoeMinus.addEventListener("click", function(){
         price()
     }      
 })
-
+//to add bag price
 backbagPlus.addEventListener("click", function(){
     document.getElementById("bag").innerText = parseInt(document.getElementById("bag").innerText)+1;
     document.getElementById("totalprice").innerText = `$${(parseInt(document.getElementById("shoe").innerText)*(74.99)+ parseInt(document.getElementById("bag").innerText)*(54.99)).toFixed(2) }`;
     price()
 })
-
+// to deduct bag price
 backbagMinus.addEventListener("click", function(){
     if (parseInt(document.getElementById("bag").innerText)>0) {
         document.getElementById("bag").innerText = parseInt(document.getElementById("bag").innerText)-1;
@@ -30,6 +32,7 @@ backbagMinus.addEventListener("click", function(){
         price()
     }      
 })
+//to show total price in hiddenprice
 function price(){
     document.getElementById("hiddenprice").value = document.getElementById("totalprice").innerText
 }
