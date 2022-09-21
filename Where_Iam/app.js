a0790daf3b926c2fd11ca61f48e2adef376f4b82
Mihail Tal <mihail.tal85@gmail.com>
2022-09-21 09:58:48 +0300

    js updated

--- Where_Iam/app.js
+++ Where_Iam/app.js
@@ -1,18 +1,22 @@
+//variables
+
 let openBtn = document.getElementById("open-btn");
 let modalContainer = document.getElementById("modal-container")
 let closeBtn = document.getElementById("close-btn")
 
 
-
+//to change container display to block
 openBtn.addEventListener("click", function(){
     modalContainer.style.display = "block";
 
 })
 
+//to change container display to none
 closeBtn.addEventListener("click", function(){
     modalContainer.style.display = "none"
 })
 
+// to change container display to none
 window.addEventListener("click", function(e){
     if (e.target ===modalContainer){
         modalContainer.style.display = "none";
